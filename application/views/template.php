228349c77c7ade1c91831a49addb3aac8cb2cf63
Muarif Gustiar Aliyudin <muarifgustiar@gmail.com>
2015-06-23 16:25:08 +0700

    print css
    print css

--- application/views/template.php
+++ application/views/template.php
@@ -11,6 +11,7 @@
 <link href="<?php echo config_item('assets'); ?>js/datepicker/css/datepicker.css" rel="stylesheet">
 <link href="<?php echo config_item('assets'); ?>css/font-awesome.css" rel="stylesheet">
 <link href="<?php echo config_item('assets'); ?>css/style.css" rel="stylesheet">
+<link href="<?php echo config_item('assets'); ?>css/print.css" rel="stylesheet">
 <link href="<?php echo config_item('assets'); ?>css/awesome-bootstrap-checkbox.css" rel="stylesheet">
 <link href="<?php echo config_item('assets'); ?>css/pages/dashboard.css" rel="stylesheet">
 <link href="<?php echo config_item('assets'); ?>js/switch/dist/css/bootstrap3/bootstrap-switch.min.css" rel="stylesheet">
